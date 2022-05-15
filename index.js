3d0efb36b0cf0f1819fba1fa3757f093742e21c7
numansaady <numansaady@gmail.com>
2022-05-15 10:18:20 +0600

    available and bookin api added

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// doctors_admin 
+// jhHdlZzYp239sJiY
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhpec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -26,10 +28,41 @@ async function run(){
       res.send(services);
     })
 
+    // Available slots for user 
+    app.get('/available', async(req, res) => {
+      const date = req.query.date;
+      // step 1: get all services
+      const services = await serviceCollection.find().toArray();
+      // step 2: get the booking of that date, output: [{},{},{},{},{},{},{},{}]
+      const query = {date: date};
+      const bookings = await bookingCollection.find(query).toArray();
+      // step 3: for each service
+      services.forEach(service => {
+        // step 4: find bookings for that service, output: [{},{},{}]
+        const serviceBookings = bookings.filter(book => book.treatment === service.name);
+        // step 5: select slots for the serviceBookings, output: ['','','']
+        const bookedSlots = serviceBookings.map(book => book.slot);
+        // step 6: select those slots these are not in bookedSlots
+        const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+        // step 7: set available to slot to make it easier 
+        service.slots = available;
+      })
+      res.send(services);
+    })
+
+    // get bookings by user email
+    app.get('/booking', async(req, res) => {
+      const patient = req.query.patient;
+      const query = {patient: patient};
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    })
+
+    // insert booking data with POST api
     app.post('/booking', async(req, res)=> {
       const booking = req.body;
       const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
-      const exists = bookingCollection.findOne(query);
+      const exists = await bookingCollection.findOne(query);
       if(exists){
         return res.send({success: false, booking:exists})
       }
