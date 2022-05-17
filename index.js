a4079ba5a8ed2fe0ae609fbaee59acd07cdd4ef2
numansaady <numansaady@gmail.com>
2022-05-17 18:25:14 +0600

    protect admin api added

--- index.js
+++ index.js
@@ -79,6 +79,13 @@ async function run() {
       res.send(services);
     });
 
+    app.get('/admin/:email', verifyJWT, async(req, res)=>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
+    })
+
     // get bookings by user email
     app.get("/booking", verifyJWT, async (req, res) => {
       const patient = req.query.patient;
